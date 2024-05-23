7b4a4f79da483f0023db43fd96a520323280fce8
Harrison-Oatman <46121828+Harrison-Oatman@users.noreply.github.com>
2024-05-23 17:36:49 -0400

    Update process_batch.sh

--- batched/process_batch.sh
+++ batched/process_batch.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH --job-name=processlightsheet         # Job name
 #SBATCH --output=output_%A_%a.out       # Standard output and error log
-#SBATCH --array=0-4                   # Array range
-#SBATCH --ntasks=4                    # Number of tasks
+#SBATCH --array=0-3                   # Array range
+#SBATCH --ntasks=1                   # Number of tasks
 #SBATCH --time=01:00:00                 # Time limit hrs:min:sec
 #SBATCH --mem=16G                       # Memory limit per task
 #SBATCH --partition=gen                # Partition name
@@ -13,4 +13,4 @@ source ~/miniconda3/etc/profile.d/conda.sh
 conda activate cellpose
 
 # Run your Python script with job_number and total_jobs
-python NucleiTracking/batched/process_data.py -i "/mnt/home/dalber/ceph/Light_Sheet_Data/240131_hisRFP_10s_2/2024-01-31_142621/Processed/" -o "/mnt/home/hoatman/ceph/lightsheet2" --rank ${SLURM_ARRAY_TASK_ID} --nprocs ${SLURM_NTASKS} --level INFO
+python NucleiTracking/batched/process_data.py -i "/mnt/home/dalber/ceph/Light_Sheet_Data/240131_hisRFP_10s_2/2024-01-31_142621/Processed/" -o "/mnt/home/hoatman/ceph/lightsheet2" --rank ${SLURM_ARRAY_TASK_ID} --nprocs 4 --level INFO
