f2768637c9d03b65fb35a65890abd8df34b84722
Harrison-Oatman <46121828+Harrison-Oatman@users.noreply.github.com>
2024-05-31 14:14:51 -0400

    Update cellpose_batch.sh

--- batched/cellpose_batch.sh
+++ batched/cellpose_batch.sh
@@ -6,7 +6,7 @@
 #SBATCH --mem=40G                            # Memory limit
 #SBATCH --partition=gpu                      # Partition name
 #SBATCH --gpus=1                             # Number of GPUs
-#SBATCH --cpus-per-task=4                    # Number of CPU cores per task
+#SBATCH --cpus-per-task=2                   # Number of CPU cores per task
 
 
 # Load your Python environment if needed
@@ -15,4 +15,4 @@ source ~/miniconda3/etc/profile.d/conda.sh
 conda activate cellpose
 
 # Run your Python script with job_number and total_jobs
-python NucleiTracking/batched/multiprocess_cellpose.py -i "ceph/3d_microscopy/tll_Embryo002b_bgs.tif" --model 3d08 --diam 17 --stitch_threshold 0.6 --use_gpu --axes tzcyx --batch_size 64 --nprocs 4 --level WARN --channels 2 0
+python NucleiTracking/batched/multiprocess_cellpose.py -i "/mnt/home/hoatman/ceph/lightsheet2/Recon_fused_tp_104_ch_0.tif" --model 3d08 --diam 17 --stitch_threshold 0.6 --use_gpu --axes zyx --batch_size 64 --nprocs 1 --level WARN --channels 0 0
