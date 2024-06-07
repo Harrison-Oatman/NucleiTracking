2452bd4b371a51e5938afb91a0e4843134cb405c
Harrison-Oatman <46121828+Harrison-Oatman@users.noreply.github.com>
2024-06-07 18:02:24 -0400

    Update process_batch.sh

--- batched/process_batch.sh
+++ batched/process_batch.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=processlightsheet         # Job name
 #SBATCH --output=output_%j.out       # Standard output and error log
 #SBATCH --ntasks=1                   # Number of tasks (cores)
-#SBATCH --time=02:30:00               # Time limit hrs:min:sec
+#SBATCH --time=08:00:00               # Time limit hrs:min:sec
 #SBATCH --mem=250G                     # Memory limit (total)
 #SBATCH --partition=gen               # Partition name
 #SBATCH --cpus-per-task=32              # Number of CPU cores per task
