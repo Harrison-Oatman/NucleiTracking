a93c24c428bbc9790a0b853fea31c1be0e3bb2c8
Harrison-Oatman <46121828+Harrison-Oatman@users.noreply.github.com>
2024-06-11 17:22:30 -0400

    Create ilastik_batch.sh

---
+++ batched/ilastik_batch.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH --job-name=processlightsheet         # Job name
+#SBATCH --output=output_%j.out       # Standard output and error log
+#SBATCH --ntasks=1                   # Number of tasks (cores)
+#SBATCH --time=08:00:00               # Time limit hrs:min:sec
+#SBATCH --mem=150G                     # Memory limit (total)
+#SBATCH --partition=gen               # Partition name
+#SBATCH --cpus-per-task=32              # Number of CPU cores per task
+
+ilastik-1.4.0.post1-Linux/run_ilastik.sh --headless \
+                   --project=process_bgs.ilp \
+                   --stack_along="t" \
+                   --output_filename_format=~/ceph/ilastik_out/{nickname}_Probabilities.h5 \
+                   "~/ceph/lightsheet2/Recon_fused_tp_*_ch_0"
