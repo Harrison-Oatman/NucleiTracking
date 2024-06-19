55b8f07b4c670f615bd653ce1b8b2655e91b4f68
Harrison-Oatman <46121828+Harrison-Oatman@users.noreply.github.com>
2024-06-19 12:56:24 -0400

    Update ilastik_batch.sh

--- batched/ilastik_batch.sh
+++ batched/ilastik_batch.sh
@@ -10,5 +10,5 @@
 ilastik-1.4.0.post1-Linux/run_ilastik.sh --headless \
                    --project=./ceph/process_bgs.ilp \
                    --stack_along="t" \
-                   --output_filename_format=./ceph/ilastik_out/{nickname}_Probabilities.h5 \
-                   "./ceph/lightsheet2/Recon_fused_tp_*_ch_0.tif"
+                   --output_filename_format=./ceph/ilastik_out/{nickname}_probabilities.h5 \
+                   "./ceph/lightsheet2/Recon_fused_tp_100_ch_0.tif"
