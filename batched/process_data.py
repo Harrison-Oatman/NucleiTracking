7ffc696672569138b4ef0eb5bf1147c749eaf8f7
Harrison-Oatman <46121828+Harrison-Oatman@users.noreply.github.com>
2024-08-30 12:38:16 -0400

    Update process_data.py

--- batched/process_data.py
+++ batched/process_data.py
@@ -11,53 +11,60 @@ import scipy.ndimage as ndi
 from scipy.ndimage._ni_support import _normalize_sequence
 
 
-def rolling_ball_filter(data, ball_radius, spacing=None, top=False, **kwargs):
-    """Rolling ball filter implemented with morphology operations
+# def rolling_ball_filter(data, ball_radius, spacing=None, top=False, **kwargs):
+#     """Rolling ball filter implemented with morphology operations
+#
+#     This implenetation is very similar to that in ImageJ and uses a top hat transform
+#     with a ball shaped structuring element
+#     https://en.wikipedia.org/wiki/Top-hat_transform
+#
+#     Parameters
+#     ----------
+#     data : ndarray
+#         image data (assumed to be on a regular grid)
+#     ball_radius : float
+#         the radius of the ball to roll
+#     spacing : int or sequence
+#         the spacing of the image data
+#     top : bool
+#         whether to roll the ball on the top or bottom of the data
+#     kwargs : key word arguments
+#         these are passed to the ndimage morphological operations
+#
+#     Returns
+#     -------
+#     data_nb : ndarray
+#         data with background subtracted
+#     bg : ndarray
+#         background that was subtracted from the data
+#     """
+#     ndim = data.ndim
+#     if spacing is None:
+#         spacing = 1
+#
+#     spacing = _normalize_sequence(spacing, ndim)
+#
+#     radius = np.asarray(_normalize_sequence(ball_radius, ndim))
+#     mesh = np.array(np.meshgrid(*[np.arange(-r, r + s, s) for r, s in zip(radius, spacing)], indexing="ij"))
+#     structure = 2 * np.sqrt(1 - ((mesh / radius.reshape(-1, *((1,) * ndim)))**2).sum(0))
+#     structure[~np.isfinite(structure)] = 0
+#     if not top:
+#         # ndi.white_tophat(data, structure=structure, output=background)
+#         background = ndi.grey_erosion(data, structure=structure, **kwargs)
+#         background = ndi.grey_dilation(background, structure=structure, **kwargs)
+#     else:
+#         # ndi.black_tophat(data, structure=structure, output=background)
+#         background = ndi.grey_dilation(data, structure=structure, **kwargs)
+#         background = ndi.grey_erosion(background, structure=structure, **kwargs)
+#
+#     return data - background, background
 
-    This implenetation is very similar to that in ImageJ and uses a top hat transform
-    with a ball shaped structuring element
-    https://en.wikipedia.org/wiki/Top-hat_transform
+START = 68
+END = 344
 
-    Parameters
-    ----------
-    data : ndarray
-        image data (assumed to be on a regular grid)
-    ball_radius : float
-        the radius of the ball to roll
-    spacing : int or sequence
-        the spacing of the image data
-    top : bool
-        whether to roll the ball on the top or bottom of the data
-    kwargs : key word arguments
-        these are passed to the ndimage morphological operations
 
-    Returns
-    -------
-    data_nb : ndarray
-        data with background subtracted
-    bg : ndarray
-        background that was subtracted from the data
-    """
-    ndim = data.ndim
-    if spacing is None:
-        spacing = 1
-
-    spacing = _normalize_sequence(spacing, ndim)
-
-    radius = np.asarray(_normalize_sequence(ball_radius, ndim))
-    mesh = np.array(np.meshgrid(*[np.arange(-r, r + s, s) for r, s in zip(radius, spacing)], indexing="ij"))
-    structure = 2 * np.sqrt(1 - ((mesh / radius.reshape(-1, *((1,) * ndim)))**2).sum(0))
-    structure[~np.isfinite(structure)] = 0
-    if not top:
-        # ndi.white_tophat(data, structure=structure, output=background)
-        background = ndi.grey_erosion(data, structure=structure, **kwargs)
-        background = ndi.grey_dilation(background, structure=structure, **kwargs)
-    else:
-        # ndi.black_tophat(data, structure=structure, output=background)
-        background = ndi.grey_dilation(data, structure=structure, **kwargs)
-        background = ndi.grey_erosion(background, structure=structure, **kwargs)
-
-    return data - background, background
+def rolling_avg(arr, window=5):
+    return np.convolve(arr, np.ones(window) / window, mode="same")
 
 
 def process_file(infile, output_dir):
@@ -72,21 +79,21 @@ def process_file(infile, output_dir):
     raw = tifffile.imread(infile)
     logging.info(f"read {infile.name}")
 
-
     # downscale by 0.5
     raw = skimage.transform.downscale_local_mean(raw, (2, 2, 2))[2:]
 
     # convert to 16-bit
-    raw = ((raw - np.quantile(raw, 0.01)) / (np.quantile(raw, .9999) - raw.min())) * 65535
-    raw = raw.clip(0, 65535)
-    raw = raw.astype(np.uint16)
-
-    # subtract background
-    # raw = np.array([rolling_ball_filter(sl, 25)[0] for sl in tqdm(raw, desc=f"subtracting background {infile.name}")])
-    # raw = raw[100:105]
+    axis_normalizer_max = rolling_avg(np.quantile(raw, 0.99, axis=(0, 1)))
+    axis_normalizer_max[:START] = axis_normalizer_max[START]
+    axis_normalizer_max[END:] = axis_normalizer_max[END]
+    axis_normalizer_min = rolling_avg(np.quantile(raw, 0.01, axis=(0, 1)))
+    axis_normalizer_min[:START] = axis_normalizer_min[START]
+    axis_normalizer_min[END:] = axis_normalizer_min[END]
+    data = (raw - axis_normalizer_min) / (axis_normalizer_max - axis_normalizer_min)
+    data = np.clip(data, 0, 1, dtype=np.float16)
 
     # save
-    tifffile.imwrite(outfile, raw, imagej=True, metadata={"axes": "zyx"})
+    tifffile.imwrite(outfile, data, imagej=True, metadata={"axes": "zyx"})
 
     logging.info(f"saved {outfile}")
 
