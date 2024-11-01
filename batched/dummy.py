829da70af99d872650187c85eb7dab693b60d697
Harrison-Oatman <46121828+Harrison-Oatman@users.noreply.github.com>
2024-11-01 14:12:30 -0400

    Update dummy.py

--- batched/dummy.py
+++ batched/dummy.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 
 def main():
-    filename = Path(r"/mnt/home/hoatman/ceph/lightsheet_20241030/raw/stack_0_channel_0_obj_left/Cam_left_00000.lux.h5")
+    filename = Path(r"/mnt/ceph/users/hoatman/lightsheet_20241030/raw/stack_0_channel_0_obj_left/Cam_left_00000.lux.h5")
 
     with h5py.File(filename, 'r') as h5_file:
         raw_vol = h5_file['/Data'][:]
