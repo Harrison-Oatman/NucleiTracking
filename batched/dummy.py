0cd127294e42fc20f0d5281f73717c3c16362eb4
Harrison-Oatman <46121828+Harrison-Oatman@users.noreply.github.com>
2024-11-01 14:16:03 -0400

    Update dummy.py

--- batched/dummy.py
+++ batched/dummy.py
@@ -1,15 +1,26 @@
 import h5py
 from pathlib import Path
+import multiprocessing
+import logging
 
 
-def main():
-    filename = Path(r"/mnt/ceph/users/hoatman/lightsheet_20241030/raw/stack_0_channel_0_obj_left/Cam_left_00000.lux.h5")
+def main(i):
+    filename = Path(f"/mnt/ceph/users/hoatman/lightsheet_20241030/raw/stack_0_channel_0_obj_left/Cam_left_0000{i}.lux.h5")
 
     with h5py.File(filename, 'r') as h5_file:
         raw_vol = h5_file['/Data'][:]
 
-    print(raw_vol.shape)
+    logging.info(raw_vol.shape)
 
 
 if __name__ == "__main__":
-    main()
+    logging.basicConfig(level=logging.INFO)
+
+    jobs = []
+
+    with multiprocessing.Pool(processes=4) as pool:
+        for i in range(10):
+            jobs.append(pool.apply_async(main, (i,)))
+
+        for job in jobs:
+            job.get()
