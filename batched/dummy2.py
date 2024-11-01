e5bd65378c61b2825cbaa6320fe2823e243118fb
Harrison-Oatman <46121828+Harrison-Oatman@users.noreply.github.com>
2024-11-01 14:40:16 -0400

    Update dummy2.py

--- batched/dummy2.py
+++ batched/dummy2.py
@@ -21,7 +21,7 @@ if __name__ == "__main__":
     with multiprocessing.Pool(processes=4) as pool:
         filepaths = list(Path(f"/mnt/ceph/users/hoatman/lightsheet_20241030/raw/stack_0_channel_0_obj_left/").glob("*.h5"))
         for f in filepaths:
-            jobs.append(pool.apply_async(main, (str(f))))
+            jobs.append(pool.apply_async(main, (str(f), )))
 
         for job in jobs:
             job.get()
