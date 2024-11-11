cce86342e496d072ada988cd4cf404788c470935
Harrison-Oatman <46121828+Harrison-Oatman@users.noreply.github.com>
2024-11-11 13:58:13 -0500

    Update patch_joiner.py

--- scripts/patch_joiner.py
+++ scripts/patch_joiner.py
@@ -25,20 +25,21 @@ def test_point_in_bounds(coords, bounds):
 #     ix = np.nonzero(masks[0])
 #     a = masks[0][ix]
 
+SOURCE = 60
 
 def main():
-    img = tifffile.imread(BASE_PATH / r"raw\Recon_fused_tp_240_ch_0_normalized.tif")
+    img = tifffile.imread(BASE_PATH / f"raw\\Recon_fused_tp_{SOURCE}_ch_0_normalized.tif")
     print(img.shape)
 
     viewer = napari.Viewer()
     img_layer = viewer.add_image(img)
     label_layer = viewer.add_labels(np.zeros_like(img, dtype=np.uint16))
 
-    with open(BASE_PATH / "patch_test_out/patches.json", "r") as f:
+    with open(BASE_PATH / f"patch_test_out/{SOURCE}/patches.json", "r") as f:
         patches = json.load(f)
 
     for patch in patches:
-        file = BASE_PATH / f"patch_test_out/patch_{patch}_cyto3masks.tif"
+        file = BASE_PATH / f"patch_test_out/{SOURCE}/patch_{patch}_cyto3masks.tif"
         mask = tifffile.imread(file)[0]
         c = [[int(val) for val in axis] for axis in patches[patch]]
         label_layer.data[c[0][0]:c[0][1], c[1][0]:c[1][1], c[2][0]:c[2][1]] += mask
