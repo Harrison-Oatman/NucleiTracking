aaa39b284b4f54e115937ce7e786a46fa185a22f
Harrison-Oatman <46121828+Harrison-Oatman@users.noreply.github.com>
2025-02-05 15:02:40 -0500

    Update dog_batch.sh

--- batched/dog_batch.sh
+++ batched/dog_batch.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=dog         # Job name
 #SBATCH --output=output_%j.out       # Standard output and error log
 #SBATCH --ntasks=1                   # Number of tasks (cores)
-#SBATCH --time=01:00:00               # Time limit hrs:min:sec
+#SBATCH --time=03:00:00               # Time limit hrs:min:sec
 #SBATCH --mem=250G                     # Memory limit (total)
 #SBATCH --partition=genx               # Partition name
 #SBATCH --cpus-per-task=32              # Number of CPU cores per task
@@ -11,4 +11,4 @@ source ~/miniconda3/etc/profile.d/conda.sh
 conda activate cellpose
 
 # Run your Python script with job_number and total_jobs
-python NucleiTracking/batched/dog.py -i "/mnt/home/hoatman/ceph/lightsheet_20241104/raw_image/downscaled/recon2/" --nprocs 32 --min_distance 3 --threshold_abs 50 --sigma_low 2 --sigma_high 7 --level INFO
+python NucleiTracking/batched/dog.py -i "/mnt/home/hoatman/ceph/lightsheet_20250131/raw_image/downscaled/recon/" --nprocs 32 --min_distance 3 --threshold_abs 50 --sigma_low 2 --sigma_high 7 --level INFO
