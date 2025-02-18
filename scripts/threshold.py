86a4d777ea7308f4d10c8f99ba687cd36e961b14
Harrison-Oatman <46121828+Harrison-Oatman@users.noreply.github.com>
2025-02-18 18:37:01 -0500

    Update threshold.py

--- scripts/threshold.py
+++ scripts/threshold.py
@@ -32,6 +32,7 @@ class JsonStoreValue:
         preset["dog"] = str(preset["dog"])
         preset["mind"] = str(preset["mind"])
         preset["value"] = int(preset["value"])
+        preset["local_value"] = int(preset["local_value"])
         self.json[stem] = preset
 
         self.save()
@@ -41,12 +42,13 @@ class JsonStoreValue:
 
 
 class ThresholdSlider(QWidget):
-    def __init__(self, update_callback, store_value_callback, dogs, mins, min_val=25, max_val=150):
+    def __init__(self, update_callback, store_value_callback, dogs, mins, min_val=25, max_val=250):
         super().__init__()
         self.update_callback = update_callback
         self.store_value_callback = store_value_callback
         self.layout = QVBoxLayout()
         self.threshold_value = 0
+        self.local_threshold_value = 0
 
         self.dog_label = QLabel("DOG preset")
         self.dog_combobox = QComboBox()
@@ -66,6 +68,14 @@ class ThresholdSlider(QWidget):
         self.threshold_slider.setValue(min_val)
         self.threshold_slider.valueChanged.connect(self.update_slider)
 
+        self.local_threshold_label = QLabel(f"Local Threshold: {min_val}")
+        self.local_threshold_slider = QSlider()
+        self.local_threshold_slider.setOrientation(1)  # Vertical slider
+        self.local_threshold_slider.setMinimum(min_val)
+        self.local_threshold_slider.setMaximum(max_val)
+        self.local_threshold_slider.setValue(min_val)
+        self.local_threshold_slider.valueChanged.connect(self.update_local_slider)
+
         self.store_value_button = QPushButton("Store Value")
         self.store_value_button.clicked.connect(self.store_value)
 
@@ -75,6 +85,8 @@ class ThresholdSlider(QWidget):
         self.layout.addWidget(self.dis_combobox)
         self.layout.addWidget(self.threshold_label)
         self.layout.addWidget(self.threshold_slider)
+        self.layout.addWidget(self.local_threshold_label)
+        self.layout.addWidget(self.local_threshold_slider)
         self.setLayout(self.layout)
 
     def update_slider(self, value):
@@ -83,10 +95,17 @@ class ThresholdSlider(QWidget):
 
         self.on_update(None)
 
-    def set_status(self, value, dog, mind):
+    def update_local_slider(self, value):
+        self.local_threshold_value = value
+        self.local_threshold_label.setText(f"Local Threshold: {value}")
+
+        self.on_update(None)
+
+    def set_status(self, value, local_value, dog, mind):
         self.dog_combobox.setCurrentText(dog)
         self.dis_combobox.setCurrentText(mind)
         self.threshold_slider.setValue(value)
+        self.local_threshold_slider.setValue(local_value)
 
         self.on_update(None)
 
@@ -94,14 +113,15 @@ class ThresholdSlider(QWidget):
         value = int(self.threshold_value)
         dog_preset = self.dog_combobox.currentText()
         dis_preset = self.dis_combobox.currentText()
-        return value, dog_preset, dis_preset
+        local_value = int(self.local_threshold_value)
+        return value, local_value, dog_preset, dis_preset
 
     def store_value(self):
         self.store_value_callback()
 
     def on_update(self, _):
-        v, dog, mind = self.get_status()
-        self.update_callback(v, dog, mind)
+        v, local_v, dog, mind = self.get_status()
+        self.update_callback(v, local_v, dog, mind)
 
 
 # def get_peaks(img, sigma_hi, sigma_low, low_threshold=20, min_distance=3):
@@ -115,13 +135,13 @@ class ThresholdSlider(QWidget):
 #     return pd.DataFrame({"z": pts[:, 0], "y": pts[:, 1], "x": pts[:, 2], "val": vals})
 
 
-def load_tiffs_and_peaks(directory):
+def load_tiffs_and_peaks(directory, peak_directory):
     tiff_files = natsorted([f for f in os.listdir(directory) if f.endswith('.tiff') or f.endswith('.tif')])
-    csv_files = natsorted([f for f in os.listdir(directory) if f.endswith('.csv')])
+    csv_files = natsorted([f for f in os.listdir(peak_directory) if f.endswith('.csv')])
 
     images = [tiff.imread(os.path.join(directory, f)) for f in tiff_files]
     images = [np.swapaxes(image, 0, 2) for image in images]
-    peak_data = [pd.read_csv(os.path.join(directory, f)) for f in csv_files]
+    peak_data = [pd.read_csv(os.path.join(peak_directory, f)) for f in csv_files]
 
     stems = [Path(f).stem for f in csv_files]
 
@@ -130,11 +150,11 @@ def load_tiffs_and_peaks(directory):
 
 class GuiProcess:
 
-    def __init__(self, directory, jsonpath):
+    def __init__(self, directory, peak_directory, jsonpath):
 
         self.directory = directory
         self.current_frame = 0
-        self.image_data, self.peak_data_list, self.stems = load_tiffs_and_peaks(directory)
+        self.image_data, self.peak_data_list, self.stems = load_tiffs_and_peaks(directory, peak_directory)
 
         jsonstorevalue = JsonStoreValue(jsonpath)
         self.store_value_callback = jsonstorevalue.update
@@ -164,9 +184,9 @@ class GuiProcess:
             self.update_frame()
 
     def store_preset(self):
-        value, dog, mind = self.slider.get_status()
+        value, local_value, dog, mind = self.slider.get_status()
         stem = self.stems[self.current_frame]
-        self.store_value_callback(stem, {"value": value, "dog": dog, "mind": mind})
+        self.store_value_callback(stem, {"value": value, "local_value": local_value, "dog": dog, "mind": mind})
 
     def update_frame(self):
         """Manually update frame display"""
@@ -177,11 +197,12 @@ class GuiProcess:
             dog = preset["dog"]
             mind = preset["mind"]
             value = preset["value"]
-            self.slider.set_status(value, dog, mind)
+            local_value = preset["local_value"]
+            self.slider.set_status(value, local_value, dog, mind)
 
         else:
-            value, dog, mind = self.slider.get_status()
-            self.update_peaks(value, dog, mind)
+            value, local_value, dog, mind = self.slider.get_status()
+            self.update_peaks(value, local_value, dog, mind)
 
         # self.scatter_layer.data = self.peak_data_list[self.current_frame][['x', 'y', 'z']].values
 
@@ -195,7 +216,7 @@ class GuiProcess:
 
         napari.run()
 
-    def update_peaks(self, value, dog_preset, min_distance_preset):
+    def update_peaks(self, value, local_value, dog_preset, min_distance_preset):
 
         data = self.peak_data_list[self.current_frame]
         print(len(data))
@@ -204,6 +225,8 @@ class GuiProcess:
         data = data[data["min-distance"].astype(str) == min_distance_preset]
         print(len(data))
         data = data[data['val'] > value]
+        print(len(data))
+        data = data[data['local'] > local_value]
         print(len(data))
 
         filtered_peaks = data[['x', 'y', 'z']].values
@@ -221,11 +244,15 @@ def main():
     directory = r"D:\Tracking\NucleiTracking\data\interim\lightsheet\2025_02_06\recon\test2"
     jsonpath = r"D:\Tracking\NucleiTracking\data\interim\lightsheet\2025_02_06\recon\test2\presets.json"
 
+    directory = r"/mnt/home/hoatman/ceph/lightsheet_20250206/raw_image/downscaled/recon/"
+    peak_directory = r"/mnt/home/hoatman/ceph/lightsheet_20250206/raw_image/downscaled/recon/dog_sweep"
+    jsonpath = r"/mnt/home/hoatman/ceph/lightsheet_20250206/raw_image/downscaled/recon/presets.json"
+
     if not Path(jsonpath).exists():
         with open(jsonpath, 'w') as f:
             json.dump({}, f)
 
-    gui_process = GuiProcess(directory, jsonpath)
+    gui_process = GuiProcess(directory, peak_directory, jsonpath)
     gui_process.run()
 
 
