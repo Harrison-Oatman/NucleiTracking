803a7f8782467e0897ce960f480b9ba99ce6226c
Harrison-Oatman <46121828+Harrison-Oatman@users.noreply.github.com>
2025-03-24 11:12:08 -0400

    Update hdf5_batch.sh

--- batched/hdf5_batch.sh
+++ batched/hdf5_batch.sh
@@ -11,4 +11,4 @@ source ~/miniforge3/etc/profile.d/conda.sh
 conda activate cellpose
 
 # Run your Python script with job_number and total_jobs
-python NucleiTracking/batched/hdf5_extraction.py -i "/mnt/home/lyang/ceph/For_Harrison/2025-03-17_142944_trk_18C/raw" -o "/mnt/home/hoatman/ceph/lightsheet_trk_20250317/raw_image" --nprocs 16 --level INFO
+python NucleiTracking/batched/hdf5_extraction.py -i "/mnt/home/lyang/ceph/For_Harrison/2025-03-18_150833_trk_18C/raw" -o "/mnt/home/hoatman/ceph/lightsheet_trk_20250318/raw_image" --nprocs 16 --level INFO
