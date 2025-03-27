25e3590ed99fa3a49ef6063a51294c3c7f8a93f4
Harrison-Oatman <46121828+Harrison-Oatman@users.noreply.github.com>
2025-03-27 13:17:17 -0400

    uv_unwrap

---
+++ batched/uv_unwrap.py
@@ -0,0 +1,123 @@
+import argparse
+import logging
+import tifffile
+import numpy as np
+from tqdm import tqdm
+import time
+import multiprocessing
+from pathlib import Path
+import natsort
+from blender_tissue_cartography import mesh as tcmesh
+from blender_tissue_cartography import interpolation as tcinterp
+
+
+def main():
+    try:
+        multiprocessing.set_start_method('spawn')
+    except RuntimeError:
+        pass
+
+    print("starting main")
+    args = process_cli()
+
+    logging.basicConfig(level=args.level)
+
+    inpath = args.input_dir
+    inpath = Path(inpath)
+    if not inpath.exists():  # try relative path
+        inpath = Path().cwd() / args.input
+
+    assert inpath.exists(), f"directory not found: {inpath}"
+    print(f"processing files in {inpath}")
+
+    outpath = args.output
+    outpath = Path(outpath) if outpath is not None else inpath.parent / f"uv_unwrap"
+    outpath.mkdir(exist_ok=True)
+    (outpath / "vals").mkdir(exist_ok=True)
+    (outpath / "locs").mkdir(exist_ok=True)
+
+    files = natsort.natsorted([f for f in inpath.iterdir() if f.suffix == '.tif'])
+    print(f"found {len(files)} tif files")
+
+    nprocs = args.nprocs
+
+    start = time.time()
+
+    with multiprocessing.Pool(processes=nprocs) as pool:
+        print(f"pool initialized in {time.time() - start} seconds")
+
+        jobs = []
+        for i, file in tqdm(enumerate(files)):
+            job = pool.apply_async(process_file, (i, str(file.absolute()), args, str(outpath.absolute())))
+            jobs.append(job)
+
+        vals_and_locs = [job.get() for job in jobs]
+
+    vals = [v for v, _ in vals_and_locs]
+    locs = [l for _, l in vals_and_locs]
+
+    v_stack = np.stack(vals, 0)
+    l_stack = np.stack(locs, 0)
+
+    tifffile.imwrite(outpath / f"all_vals.tif", v_stack)
+    tifffile.imwrite(outpath / f"all_locs.tif", np.array(l_stack, dtype=int))
+
+
+def process_cli() -> argparse.Namespace:
+    argparser = argparse.ArgumentParser(description="script to process raw data from tif")
+
+    argparser.add_argument("-i", "--input_dir", dest="input_dir", help="path to raw file to process", default=None)
+    argparser.add_argument("-o", "--output", dest="output", help="results directory", default=None)
+    argparser.add_argument("--obj", help="path to blender object file", default=None)
+
+    argparser.add_argument_group("box")
+    argparser.add_argument("--range", nargs=3, type=int, default=[0, 0, 1])
+    argparser.add_argument("--resolution", nargs=3, type=int, default=[1, 1, 1])
+    argparser.add_argument("--uv_grid_steps", type=int, default=2048)
+
+    argparser.add_argument_group("multiprocessing")
+    argparser.add_argument("--nprocs", default=1, type=int)
+
+    argparser.add_argument_group("other")
+    argparser.add_argument("-l", "--level", default="INFO")
+
+    return argparser.parse_args()
+
+
+def process_file(j, infile, args, outpath):
+    logging.basicConfig(level=args.level)
+
+    infile = Path(infile)
+    outpath = Path(outpath)
+
+    logging.info(f"processing file {infile.stem} on iter {j}")
+
+    mapping_arr = tifffile.imread(str(infile))
+
+    mesh_uv = tcmesh.ObjMesh.read_obj(args.obj)
+    normal_offsets = np.linspace(args.range[0], args.range[1], args.range[2])
+
+    projected_data, projected_coordinates, projected_normals = tcinterp.create_cartographic_projections(
+        image=np.expand_dims(mapping_arr, axis=0),
+        mesh=mesh_uv,
+        resolution=(args.resolution[0], args.resolution[1], args.resolution[2]),
+        normal_offsets=normal_offsets,
+        uv_grid_steps=args.uv_grid_steps,)
+
+    val = np.max(projected_data[0], axis=0)
+    argmax = np.argmax(projected_data[0], axis=0)
+
+    loc = projected_coordinates + projected_normals * normal_offsets[argmax]
+
+    val_outfile = outpath / "vals" / f"{infile.stem}_unwrap_max_project.tif"
+    loc_outfile = outpath / "locs" / f"{infile.stem}_unwrap_locs.tif"
+
+    # save the results
+    tifffile.imwrite(val_outfile, val)
+    tifffile.imwrite(loc_outfile, np.array(loc))
+
+    return val, loc
+
+
+if __name__ == "__main__":
+    main()
---
+++ batched/uv_unwrap.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH --job-name=convert_hdf5         # Job name
+#SBATCH --output=output_%j.out          # Standard output and error log
+#SBATCH --ntasks=1                      # Number of tasks (cores)
+#SBATCH --time=08:00:00                 # Time limit hrs:min:sec
+#SBATCH --mem=250G                      # Memory limit (total)
+#SBATCH --partition=genx                # Partition name
+#SBATCH --cpus-per-task=16              # Number of CPU cores per task
+
+source ~/miniforge3/etc/profile.d/conda.sh
+conda activate cellpose
+
+# Run your Python script with job_number and total_jobs
+python NucleiTracking/batched/uv_unwrap.py -i "/mnt/home/hoatman/ceph/lightsheet_trk_20250318/raw_image/downscaled/recon" --obj "/mnt/home/hoatman/ceph/lightsheet_trk_20250318/raw_image/downscaled/recon/mesh_with_UV.obj" --nprocs 16 --range "-5" 5 11
