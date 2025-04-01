ff896835fd3d2fd1fca5304af8260a642775010e
Harrison-Oatman <46121828+Harrison-Oatman@users.noreply.github.com>
2025-04-01 14:23:45 -0400

    Update uv_unwrap.sh

--- batched/uv_unwrap.sh
+++ batched/uv_unwrap.sh
@@ -11,4 +11,4 @@ source ~/miniforge3/etc/profile.d/conda.sh
 conda activate cellpose
 
 # Run your Python script with job_number and total_jobs
-python NucleiTracking/batched/uv_unwrap.py -i "/mnt/home/hoatman/ceph/lightsheet_trk_20250319a/raw_image/downscaled/recon" --obj "/mnt/home/hoatman/ceph/lightsheet_trk_20250319a/raw_image/downscaled/recon/large.obj" --nprocs 16 --range "-20" 2 12
+python NucleiTracking/batched/uv_unwrap.py -i "/mnt/home/hoatman/ceph/lightsheet_trk_20250319b/raw_image/downscaled/recon" --obj "/mnt/home/hoatman/ceph/lightsheet_trk_20250319b/raw_image/downscaled/recon/small.obj" --nprocs 16 --range "-10" 2 13
