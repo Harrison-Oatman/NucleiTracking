1a6dbcced7a7fd1261208f19433d4daec2c591b5
Harrison-Oatman <46121828+Harrison-Oatman@users.noreply.github.com>
2025-04-02 17:36:01 -0400

    Update cellpose_crop.py

--- scripts/cellpose_crop.py
+++ scripts/cellpose_crop.py
@@ -1,9 +1,10 @@
 import numpy as np
 import tifffile
 from pathlib import Path
+from tqdm import tqdm
 
 
-folder = Path(r"D:\Tracking\NucleiTracking\data\interim\lightsheet\2025_02_06\recon\box_project")
+folder = Path(r"D:\Tracking\NucleiTracking\data\interim\lightsheet\2025_03_18_trk\uv_unwrap")
 out = folder / "train"
 
 random_crop_size = 224
@@ -11,21 +12,34 @@ random_crop_size = 224
 
 def main():
 
-    files = folder.glob("*tif")
+    files = [folder / "all_vals.tif"]
 
     for f in files:
         raw = tifffile.imread(f)
         shape = raw.shape
 
-        for t in range(shape[0]):
+        for t in tqdm(range(shape[0])):
             frame = raw[t]
 
-            y_start = np.random.randint(0, shape[1] - random_crop_size)
-            y_end = y_start + random_crop_size
-            x_start = np.random.randint(0, shape[2] - random_crop_size)
-            x_end = x_start + random_crop_size
+            minv = np.min(frame[frame > 0])
+            maxv = np.max(frame[frame > 0])
+            frame = (frame - minv) / (maxv - minv) * 255
+            frame = frame.astype(np.uint8)
 
-            tifffile.imwrite(out / f"{f.stem}_t{t}_random_crop.tif", frame[y_start:y_end, x_start:x_end])
+            for k in range(4):
+
+                h = shape[1] // 2
+                w = shape[2] // 2
+
+                row = k // 2
+                col = k % 2
+
+                y_start = np.random.randint(h*row, h*(row + 1) - random_crop_size)
+                y_end = y_start + random_crop_size
+                x_start = np.random.randint(w*col, w*(col + 1) - random_crop_size)
+                x_end = x_start + random_crop_size
+
+                tifffile.imwrite(out / f"{f.stem}_t{t}_random_crop_{k}.tif", frame[y_start:y_end, x_start:x_end])
 
 
 if __name__ == "__main__":
