19c0427bfb776733a3e0543848b9925e6fc5bf47
Harrison-Oatman <46121828+Harrison-Oatman@users.noreply.github.com>
2025-05-28 19:22:47 -0400

    centroids

---
+++ batched/find_centroids.py
@@ -0,0 +1,87 @@
+import tifffile
+import numpy as np
+from skimage.measure import regionprops_table
+from pathlib import Path
+from tqdm import tqdm
+import re
+import pandas as pd
+
+def find_centroids(masks, locs):
+
+    props = regionprops_table(masks, locs, properties=("centroid", "intensity_mean"))
+
+    return pd.DataFrame(props)
+
+def main():
+    args = process_cli()
+
+    base = Path(args.base)
+
+    masks_path = base / "cellpose_output"
+
+    # regex get mesh name and timepoint
+    # sample filename: bottom_recon_fused_tp_366_ch_0_unwrap_cp_masks.tif
+    pattern = re.compile(r"(?P<mesh_name>.+)_tp_(?P<timepoint>\d+)_ch_0_unwrap_cp_masks\.tif")
+    masks_files = list(masks_path.glob("*.tif"))
+
+    all_files = []
+
+    # infer mesh names from directory names
+    mesh_names = set()
+
+    for file in masks_files:
+        match = pattern.match(file.stem)
+        if not match:
+            continue
+
+        mesh_name = match.group("mesh_name")
+        timepoint = match.group("timepoint")
+
+        mesh_names.add(mesh_name)
+        all_files.append((mesh_name, timepoint, file))
+
+    locs = {}
+
+    for mesh_name in mesh_names:
+        locs_file = base / mesh_name / "locs" / f"{mesh_name}_all_locs.tif"
+        if locs_file.exists():
+            locs[mesh_name] = tifffile.imread(locs_file)
+        else:
+            print(f"Locs file not found for {mesh_name}: {locs_file}")
+
+    all_props = []
+
+    for mesh_name, timepoint, masks_file in tqdm(all_files):
+        if mesh_name not in locs:
+            print(f"Skipping {mesh_name} as locs file is missing.")
+            continue
+
+        masks = tifffile.imread(masks_file)
+        locs_data = locs[mesh_name]
+
+        props = find_centroids(masks, locs_data)
+
+        props["mesh_name"] = mesh_name
+        props["timepoint"] = timepoint
+
+        all_props.append(props)
+
+    if all_props:
+        all_props_df = pd.concat(all_props, ignore_index=True)
+        output_file = base / "centroids.csv"
+        all_props_df.to_csv(output_file, index=False)
+        print(f"Centroids saved to {output_file}")
+
+
+def process_cli():
+    import argparse
+
+    parser = argparse.ArgumentParser(description="Find centroids in masks and locs.")
+    parser.add_argument("--base", type=str, required=True, help="Base uv_unwrap directory.")
+
+    args = parser.parse_args()
+
+    return args
+
+if __name__ == "__main__":
+    main()
---
+++ batched/find_centroids.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH --job-name=uv_unwrap         # Job name
+#SBATCH --output=output_%j.out          # Standard output and error log
+#SBATCH --ntasks=1                      # Number of tasks (cores)
+#SBATCH --time=08:00:00                 # Time limit hrs:min:sec
+#SBATCH --mem=250G                      # Memory limit (total)
+#SBATCH --partition=genx                # Partition name
+#SBATCH --cpus-per-task=128              # Number of CPU cores per task
+
+source ~/miniforge3/etc/profile.d/conda.sh
+conda activate cellpose
+
+# Run your Python script with job_number and total_jobs
+python NucleiTracking/batched/find_centroids.py --base "/mnt/home/hoatman/ceph/lightsheet_20250206/raw_image/downscaled/uv_unwrap/"
