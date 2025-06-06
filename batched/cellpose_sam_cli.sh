13fe96bf8e4d49e1d624606cc6ccabf37edc7fc1
Harrison-Oatman <46121828+Harrison-Oatman@users.noreply.github.com>
2025-06-06 15:18:58 -0400

    Create cellpose_sam_cli.sh

---
+++ batched/cellpose_sam_cli.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+#SBATCH --job-name=ls_batch                  # Job name
+#SBATCH --output=output_%j.out               # Standard output and error log
+#SBATCH --ntasks=1                           # Number of tasks
+#SBATCH --time=08:00:00                      # Time limit hrs:min:sec
+#SBATCH --mem=750G                            # Memory limit
+#SBATCH --partition=gpu                      # Partition name
+#SBATCH --gpus=4                             # Number of GPUs
+#SBATCH --cpus-per-task=8                    # Number of CPU cores per task
+
+echo "starting task"
+
+# Load Python environment
+module load modules/2.2-20230808
+module load cuda/12.1.1
+source ~/miniforge3/etc/profile.d/conda.sh
+export PATH="$HOME/bin:$PATH"
+conda activate cellpose_sam
+
+TOPDIR="/mnt/home/hoatman/ceph/lightsheet_20250206/raw_image/downscaled/uv_unwrap/"
+SAVEDIR="${TOPDIR}/cellpose_output"
+
+export SAVEDIR
+
+mkdir -p "$SAVEDIR"
+
+# GPU list (0,1,2)
+GPUS=(0 1 2)
+
+run_cellpose_file() {
+    file=$1
+    jobnum=$2
+    gpu=$(( (jobnum - 1) % 3 ))
+    echo "Processing $file on GPU $gpu"
+    echo "$SAVEDIR"
+    python -m cellpose --image_path "$file" --pretrained_model uv_sam_001 --diameter 11.54 --use_gpu --save_tif --verbose --norm_percentile 0 100 --no_npy --savedir "$SAVEDIR" --gpu $gpu
+}
+export -f run_cellpose_file
+
+# Run on *loc.tif in top-level directory
+echo "Running cellpose.py on top-level *vals.tif files..."
+find "$TOPDIR" -maxdepth 1 -type f -name '*vals.tif' | \
+    parallel --jobs 3 --ungroup --env run_cellpose_file --env SAVEDIR \
+    'run_cellpose_file {} {#}'
+
+
+run_cellpose_dir() {
+    dir=$1
+    jobnum=$2
+    gpu=$(( (jobnum - 1) % 3 ))
+    python -m cellpose --dir "$dir" --pretrained_model uv_sam_001 --diameter 11.54 --use_gpu --save_tif --verbose --norm_percentile 0 100 --no_npy --savedir "$SAVEDIR" --gpu $gpu --stitch_threshold 0.25
+}
+export -f run_cellpose_dir
+
+# Run in each subdirectory of TOPDIR
+echo "Running cellpose.py in subdirectories..."
+find "$TOPDIR" -mindepth 2 -maxdepth 2 -name 'vals' -type d | \
+    parallel --jobs 3 --ungroup --env run_cellpose_dir --env SAVEDIR \
+    'run_cellpose_dir {} {#}'
+
+
