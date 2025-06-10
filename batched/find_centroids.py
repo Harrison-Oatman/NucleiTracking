76521ed6438c56c3fa90466bdbf75c7216641299
Harrison-Oatman <46121828+Harrison-Oatman@users.noreply.github.com>
2025-06-09 22:24:53 -0400

    Update find_centroids.py

--- batched/find_centroids.py
+++ batched/find_centroids.py
@@ -72,98 +72,103 @@ def main():
     """
     2D centroids
     """
+    #
+    # masks_files = list(masks_path.glob("*.tif"))
+    #
+    # pattern_2d = re.compile(r"(?P<mesh_name>.+)_all_vals_cp_masks")
+    # pattern_3d = re.compile(r"(?P<mesh_name>.+)_recon_fused_tp_(?P<timepoint>\d+)_ch_0_unwrap_cp_masks")
+    #
+    # all_files_2d = []
+    # all_files_3d = []
+    #
+    # mesh_names = set()
+    #
+    # for file in masks_files:
+    #     match = pattern_2d.match(file.stem)
+    #
+    #     if match:
+    #         mesh_name = match.group("mesh_name")
+    #         mesh_names.add(mesh_name)
+    #         all_files_2d.append((mesh_name, file))
+    #
+    #     match = pattern_3d.match(file.stem)
+    #
+    #     if match:
+    #         mesh_name = match.group("mesh_name")
+    #         timepoint = match.group("timepoint")
+    #
+    #         mesh_names.add(mesh_name)
+    #         all_files_3d.append((mesh_name, timepoint, file))
+    #
+    # all_props = []
+    #
+    # for mesh, mask_file in all_files_2d:
+    #
+    #     masks = tifffile.imread(mask_file)
+    #     locs = tifffile.imread(base / f"{mesh}_all_locs.tif")
+    #     vals = tifffile.imread(base / f"{mesh}_all_vals.tif")
+    #     args = tifffile.imread(base / f"{mesh}_all_vals_max_project.tif")
+    #
+    #     props = find_centroids_2d(masks, locs, vals, args)
+    #     props["mesh_name"] = mesh
+    #     all_props.append(props)
+    #
+    # if all_props:
+    #     all_props_df = pd.concat(all_props, ignore_index=True)
+    #     output_file = base / "centroids_2d.csv"
+    #     all_props_df.to_csv(output_file, index=False)
+    #     print(f"2D centroids saved to {output_file}")
+    #
 
-    masks_files = list(masks_path.glob("*.tif"))
+    """
+    2D centroids
+    """
 
-    pattern_2d = re.compile(r"(?P<mesh_name>.+)_all_vals_cp_masks")
-    pattern_3d = re.compile(r"(?P<mesh_name>.+)_recon_fused_tp_(?P<timepoint>\d+)_ch_0_unwrap_cp_masks")
-
-    all_files_2d = []
-    all_files_3d = []
-
-    mesh_names = set()
-
-    for file in masks_files:
-        match = pattern_2d.match(file.stem)
-
-        if match:
-            mesh_name = match.group("mesh_name")
-            mesh_names.add(mesh_name)
-            all_files_2d.append((mesh_name, file))
-
-        match = pattern_3d.match(file.stem)
-
-        if match:
-            mesh_name = match.group("mesh_name")
-            timepoint = match.group("timepoint")
-
-            mesh_names.add(mesh_name)
-            all_files_3d.append((mesh_name, timepoint, file))
-
-    all_props = []
-
-    for mesh, mask_file in all_files_2d:
-
-        masks = tifffile.imread(mask_file)
-        locs = tifffile.imread(base / f"{mesh}_all_locs.tif")
-        vals = tifffile.imread(base / f"{mesh}_all_vals.tif")
-        args = tifffile.imread(base / f"{mesh}_all_vals_max_project.tif")
-
-        props = find_centroids_2d(masks, locs, vals, args)
-        props["mesh_name"] = mesh
-        all_props.append(props)
-
-    if all_props:
-        all_props_df = pd.concat(all_props, ignore_index=True)
-        output_file = base / "centroids_2d.csv"
-        all_props_df.to_csv(output_file, index=False)
-        print(f"2D centroids saved to {output_file}")
-
-
-    locs = {}
-
-    for mesh_name in mesh_names:
-        locs_file = base / mesh_name / "locs" / f"{mesh_name}_full_locs.tif"
-        if locs_file.exists():
-            full_locs = tifffile.imread(locs_file)
-            dis = distance_transform_edt(1 - np.isnan(full_locs[..., 0]))
-
-            full_locs = np.concatenate([full_locs, np.expand_dims(dis, -1)], axis=-1)
-
-            locs[mesh_name] = full_locs
-
-        else:
-            print(f"Locs file not found for {mesh_name}: {locs_file}")
-
-    all_props = []
-
-    for mesh_name, timepoint, masks_file in tqdm(all_files_3d):
-        if mesh_name not in locs:
-            print(f"Skipping {mesh_name} as locs file is missing.")
-            continue
-
-        masks = tifffile.imread(masks_file)
-        locs_data = locs[mesh_name]
-
-        props = find_centroids_3d(masks, locs_data)
-
-        props["mesh_name"] = mesh_name
-        props["timepoint"] = timepoint
-
-        all_props.append(props)
-
-    if all_props:
-        all_props_df = pd.concat(all_props, ignore_index=True)
-        output_file = base / "centroids_3d.csv"
-        all_props_df.to_csv(output_file, index=False)
-        print(f"Centroids saved to {output_file}")
+    #
+    # locs = {}
+    #
+    # for mesh_name in mesh_names:
+    #     locs_file = base / mesh_name / "locs" / f"{mesh_name}_full_locs.tif"
+    #     if locs_file.exists():
+    #         full_locs = tifffile.imread(locs_file)
+    #         dis = distance_transform_edt(1 - np.isnan(full_locs[..., 0]))
+    #
+    #         full_locs = np.concatenate([full_locs, np.expand_dims(dis, -1)], axis=-1)
+    #
+    #         locs[mesh_name] = full_locs
+    #
+    #     else:
+    #         print(f"Locs file not found for {mesh_name}: {locs_file}")
+    #
+    # all_props = []
+    #
+    # for mesh_name, timepoint, masks_file in tqdm(all_files_3d):
+    #     if mesh_name not in locs:
+    #         print(f"Skipping {mesh_name} as locs file is missing.")
+    #         continue
+    #
+    #     masks = tifffile.imread(masks_file)
+    #     locs_data = locs[mesh_name]
+    #
+    #     props = find_centroids_3d(masks, locs_data)
+    #
+    #     props["mesh_name"] = mesh_name
+    #     props["timepoint"] = timepoint
+    #
+    #     all_props.append(props)
+    #
+    # if all_props:
+    #     all_props_df = pd.concat(all_props, ignore_index=True)
+    #     output_file = base / "centroids_3d.csv"
+    #     all_props_df.to_csv(output_file, index=False)
+    #     print(f"Centroids saved to {output_file}")
 
     # alternative approach, look for 2d stack as slices in subfolder
     meshes = set()
 
     locs_pattern = re.compile(r"(?P<mesh_name>.+)_full_locs.tif")
 
-    for locs_file in base.glob("full_locs.tif"):
+    for locs_file in base.glob("*full_locs.tif"):
 
         match = locs_pattern.match(locs_file.stem)
         if match:
