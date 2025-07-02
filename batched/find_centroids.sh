eb3479e069ea173a0ff2351cfa430ebc28641583
Harrison-Oatman <46121828+Harrison-Oatman@users.noreply.github.com>
2025-07-02 13:43:57 -0400

    Update find_centroids.sh

--- batched/find_centroids.sh
+++ batched/find_centroids.sh
@@ -11,4 +11,4 @@ source ~/miniforge3/etc/profile.d/conda.sh
 conda activate cellpose
 
 # Run your Python script with job_number and total_jobs
-python NucleiTracking/batched/find_centroids.py --base "/mnt/home/hoatman/ceph/lightsheet_trk_20250325/raw_image/downscaled/uv_unwrap/"
+python NucleiTracking/batched/find_centroids.py --base "/mnt/home/hoatman/ceph/lightsheet_trk_20250321/raw_image/downscaled/uv_unwrap/"
