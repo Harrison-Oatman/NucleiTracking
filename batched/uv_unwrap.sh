519b0dab0a0e8bcf7c20789d09de0405d632f5ee
Harrison-Oatman <46121828+Harrison-Oatman@users.noreply.github.com>
2025-07-04 12:08:53 -0400

    Update uv_unwrap.sh

--- batched/uv_unwrap.sh
+++ batched/uv_unwrap.sh
@@ -5,7 +5,7 @@
 #SBATCH --time=08:00:00                 # Time limit hrs:min:sec
 #SBATCH --mem=250G                      # Memory limit (total)
 #SBATCH --partition=genx                # Partition name
-#SBATCH --cpus-per-task=16              # Number of CPU cores per task
+#SBATCH --cpus-per-task=128            # Number of CPU cores per task
 
 source ~/miniforge3/etc/profile.d/conda.sh
 conda activate cellpose
