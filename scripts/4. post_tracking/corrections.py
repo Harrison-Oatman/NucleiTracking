13aef1c0bcabac17ededcbb995a62a379d584939
Harrison-Oatman <46121828+Harrison-Oatman@users.noreply.github.com>
2025-10-27 19:51:44 -0400

    tracking corrections script

---
+++ scripts/4. post_tracking/corrections.py
@@ -0,0 +1,25 @@
+import napari
+from nucleitracking.utils.load_hdf5_data import load_embryo
+from pathlib import Path
+
+spots_path = Path(r"D:\Tracking\NucleiTracking\data\processed\lightsheet\spots")
+embryo = r"lightsheet_20250131_spots.h5"
+spots_df = load_embryo(spots_path / embryo)[2]
+
+def main():
+    viewer = napari.Viewer()
+    points_layer = viewer.add_points(
+        spots_df[["t", "x", "y", "z"]].values,
+        size=2,
+        face_color="red",
+        name="nuclei",
+        ndim=3,
+    )
+    tracks_layer = viewer.add_tracks(
+        spots_df[["t", "track_id", "x", "y", "z"]].values,
+        name="tracks",
+    )
+    napari.run()
+
+if __name__ == "__main__":
+    main()
