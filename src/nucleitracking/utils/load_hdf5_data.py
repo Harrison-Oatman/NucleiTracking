ed77d1f154ee5641f2acde1a961e58516a23147f
Harrison-Oatman <46121828+Harrison-Oatman@users.noreply.github.com>
2025-11-24 21:11:21 -0500

    update imports

--- src/nucleitracking/utils/load_hdf5_data.py
+++ src/nucleitracking/utils/load_hdf5_data.py
@@ -40,6 +40,7 @@ def load_embryo(spots_path, source_index=None):
     """
     spots_df = pd.read_hdf(spots_path, key="df")
     spots_df["source"] = source_index
+    print(spots_df.columns)
     """
         Get metadata
         """
@@ -47,30 +48,5 @@ def load_embryo(spots_path, source_index=None):
     with File(spots_path, "r") as f:
         m = f["metadata"]
         metadata.update(m.attrs)
-    """
-        Include dynamic time warp data
-        """
-    dtw_path = spots_path.parent / "dtw" / f"{spots_path.stem}_dtw.h5"
-    if dtw_path.exists():
-        dtw_update = pd.read_hdf(dtw_path, key="df")
-        spots_df = pd.merge(
-            spots_df, dtw_update, left_index=True, right_index=True, how="left"
-        )
-        spots_df["cycle_pseudotime"] = spots_df["pseudotime"] + spots_df["cycle"] - 11
-    else:
-        spots_df["distance"] = np.nan  # rename this later
-        spots_df["pseudotime"] = np.nan
-    """
-        Calculate displacement
-        """
-    cols = ["x", "y", "z", "AP", "theta"]
-    for col in cols:
-        spots_df[f"d{col}"] = (
-            spots_df[col] - spots_df["parent_id"].map(spots_df[col])
-        ) / metadata["seconds_per_frame"]
-    spots_df["dtot"] = np.sqrt(
-        spots_df["dx"] ** 2 + spots_df["dy"] ** 2 + spots_df["dz"] ** 2
-    )
-    spots_df["dAP_abs"] = spots_df["dAP"].abs()
 
     return spots_path.stem, metadata, spots_df
